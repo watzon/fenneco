7f01ed9898ec590f8cdf8a5f89e07a52db74d947
Chris Watson <cawatson1993@gmail.com>
2020-06-17 21:51:52 -0600

    Disable bot tokens listener

--- src/fenneco/modules/listeners/bot_tokens.cr
+++ src/fenneco/modules/listeners/bot_tokens.cr
@@ -1,12 +1,12 @@
-class Fenneco < Proton::Client
-  @[Help(
-    description: "Listens for bot tokens posted in @BotSupport and replies with a photo."
-  )]
-  @[OnMessage(Fenneco::Utils::BOT_TOKEN_RE, outgoing: false)]
-  def bot_tokens_listener(ctx)
-    msg = ctx.message
-    if ctx.message.chat_id == -1001311056733
-      send_message(msg.chat_id!, file: "src/fenneco/images/days-since-last-bot-token.jpg", reply_message: msg)
-    end
-  end
-end
+# class Fenneco < Proton::Client
+#   @[Help(
+#     description: "Listens for bot tokens posted in @BotSupport and replies with a photo."
+#   )]
+#   @[OnMessage(Fenneco::Utils::BOT_TOKEN_RE, outgoing: false)]
+#   def bot_tokens_listener(ctx)
+#     msg = ctx.message
+#     if ctx.message.chat_id == -1001311056733
+#       send_message(msg.chat_id!, file: "src/fenneco/images/days-since-last-bot-token.jpg", reply_message: msg)
+#     end
+#   end
+# end
